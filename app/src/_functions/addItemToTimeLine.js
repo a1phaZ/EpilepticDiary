f13084b07f43b4d26719874a60a36d3732d2eb37
Artemiy Zebzeev <artemiy.zebzeev@gmail.com>
2021-03-26 11:12:25 +0500

    [func] Add handlers function addItemToTimeLine

---
+++ app/src/_functions/addItemToTimeLine.js
@@ -0,0 +1,23 @@
+import {store} from "../index";
+import {disableButton, enableButton} from "../store/buttonsPanel/actions";
+
+export default function addItemToTimeLine (e) {
+	const {type, subType, color} = e.currentTarget.dataset;
+	const {disabled} = e.currentTarget.disabled;
+	const item = {
+		type: type,
+		subType: subType,
+		time: new Date().getTime(),
+		color: `badge-${color}`
+	};
+	console.log(item);
+	if (type === 'Сон') {
+		if (!disabled) {
+			store.dispatch(disableButton(type))
+			//TODO store.dispatch(addSleepToTimeLine(item))
+		} else {
+			//TODO store.dispatch(hideSleepEndButton())
+			store.dispatch(enableButton(type))
+		}
+	}
+}
