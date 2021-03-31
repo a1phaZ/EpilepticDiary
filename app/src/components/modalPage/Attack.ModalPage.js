5a4f6172ad79f9005f93d4ff32dbfbc2b71be817
Artemiy Zebzeev <artemiy.zebzeev@gmail.com>
2021-03-31 13:55:14 +0500

    [design] re-design

--- app/src/components/modalPage/Attack.ModalPage.js
+++ app/src/components/modalPage/Attack.ModalPage.js
@@ -1,10 +1,37 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {Button, Form, Modal} from "react-bootstrap";
 
 export const Attack = ({handleClose, handleSubmit}) => {
 	const [count, setCount] = useState(1);
 	const [series, setSeries] = useState(false);
 	const [strength, setStrength] = useState(1);
+	const [rangeDescription, setRangeDescription] = useState('Слабый приступ');
+	const [textClass, setTextClass] = useState('secondary');
+	
+	useEffect(() => {
+		switch (strength) {
+			case '1': {
+				setTextClass('secondary')
+				setRangeDescription('Слабый приступ');
+				return;
+			}
+			case '2': {
+				setTextClass('warning')
+				setRangeDescription('Средний приступ');
+				return;
+			}
+			case '3': {
+				setTextClass('danger')
+				setRangeDescription('Сильный приступ');
+				return;
+			}
+			default: {
+				setTextClass('secondary')
+				setRangeDescription('Слабый приступ');
+				return;
+			}
+		}
+	}, [strength]);
 	
 	return (
 		<>
@@ -16,24 +43,29 @@ export const Attack = ({handleClose, handleSubmit}) => {
 							id={'checkbox-series'}
 							label={'Серийные приступы?'}
 							checked={series}
-							onChange={() => {setSeries(!series)}}
+							onChange={() => {
+								setSeries(!series)
+							}}
 						/>
 					</Form.Group>
 					
 					{series && <Form.Group controlId={'formAttackCount'}>
-						<Form.Label>Кол-во приступов</Form.Label>
-						<Form.Control type={'number'} placeholder={'Кол-во приступов'} value={count} onChange={(e) => {setCount(e.currentTarget.value)}}/>
+						<Form.Control type={'number'} placeholder={'Кол-во приступов'} value={count} onChange={(e) => {
+							setCount(e.currentTarget.value)
+						}}/>
 					</Form.Group>}
 					
 					<Form.Group controlId="formAttackStrength">
-						<Form.Label>Range</Form.Label>
-						<Form.Control type="range" min={1} max={3} value={strength} step={1} onChange={(e) => setStrength(e.currentTarget.value)}/>
+						<Form.Label>{`Сила приступа:`}</Form.Label>
+						<Form.Control type="range" min={1} max={3} value={strength} step={1}
+													onChange={(e) => setStrength(e.currentTarget.value)}/>
+						<Form.Text className={`text-${textClass}`}>{rangeDescription}</Form.Text>
 					</Form.Group>
 				</Form>
 			</Modal.Body>
 			<Modal.Footer>
 				<Button variant="secondary" onClick={handleClose}>
-					Закрыть
+					Отмена
 				</Button>
 				<Button variant="primary" onClick={() => {
 					const item = {
@@ -41,7 +73,7 @@ export const Attack = ({handleClose, handleSubmit}) => {
 						count: count,
 						series: series,
 						strength: strength,
-						color: 'badge-danger'
+						color: 'badge-warning'
 					};
 					handleSubmit(item)
 				}}>
