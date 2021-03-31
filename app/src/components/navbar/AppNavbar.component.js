7e99ea2d048f80a410ef486b85c5e3b6873d3910
Artemiy Zebzeev <artemiy.zebzeev@gmail.com>
2021-03-31 13:58:26 +0500

    [design] add react router

---
+++ app/src/components/navbar/AppNavbar.component.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {Nav, Navbar} from "react-bootstrap";
+import {Link} from "react-router-dom";
+
+export default function AppNavbar() {
+	return (
+		<Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+			<Link className={'navbar-brand'} to="/">Дневник</Link>
+			<Navbar.Toggle aria-controls="responsive-navbar-nav"/>
+			<Navbar.Collapse id="responsive-navbar-nav">
+				<Nav>
+					<Link className={'nav-link'} to="/settings">Настройки</Link>
+					<Link className={'nav-link'} to="/stats" disabled>Статистика</Link>
+				</Nav>
+			</Navbar.Collapse>
+		</Navbar>
+	)
+}
