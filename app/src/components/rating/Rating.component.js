be96d526b680f6e1a810c60c88cf44c113a731d8
Artemiy Zebzeev <artemiy.zebzeev@gmail.com>
2021-03-31 13:58:41 +0500

    [design] re-design

--- app/src/components/rating/Rating.component.js
+++ app/src/components/rating/Rating.component.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './rating.styles.css';
 
-const ratingArray = [1, 2, 3, 4, 5];
+const ratingArray = [1, 2, 3];
 const Rating = ({count}) => {
 	const _renderedStars = ratingArray.map((item) => {
 		return (<span key={item} className={`fa fa-star ${item <= count && 'checked'}`}/>)
